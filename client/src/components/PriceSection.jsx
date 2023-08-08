66d1d5785454711ee6d3e6bcaa11743ad5901461
Abis <abisali@hotmail.com>
2023-08-08 21:47:57 +0400

    Update PriceSection.jsx

--- client/src/components/PriceSection.jsx
+++ client/src/components/PriceSection.jsx
@@ -8,22 +8,12 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 let PriceRow = () => {
   return (
-    // <Container fluid>
-    //   <Row>
-    //     <Col lg={6} sm={12}>
-    //       <CardBox />
-    //     </Col>
-    //     <Col lg={6} sm={12}>
-    //       {/* <CardBox /> */}
-    //     </Col>
-    //   </Row>
-    // </Container>
     <div className="group-four">
             <Row xs={1} md={2} className="g-4">
       {Array.from({ length: 4 }).map((_, idx) => (
         <Col key={idx}>
           <Card>
-            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22453%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20453%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189d6157e85%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A23pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189d6157e85%22%3E%3Crect%20width%3D%22453%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22167.6796875%22%20y%3D%2290.2%22%3E453x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+            <Card.Img variant="top" src="https://i.imgur.com/fQnAmTV.png" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -38,8 +28,6 @@ let PriceRow = () => {
     </Row>
     </div>
  
-
-  
   );
 };
 
