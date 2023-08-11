d1bc0efc8aa433048e1ad5a322efc5655c9275fd
Abis <abisali@hotmail.com>
2023-08-11 22:56:48 +0400

    Update TableSection.jsx

--- client/src/components/TableSection.jsx
+++ client/src/components/TableSection.jsx
@@ -2,51 +2,6 @@ import React from "react";
 import { Container, Table, Row, Col } from "react-bootstrap";
 
 const FuelPriceTable = (props) => {
-  const data = [
-    {
-      date: "March 2023",
-      super98: "3.09",
-      special95: "2.97",
-      ePlus91: "2.90",
-      diesel: "3.14",
-    },
-    {
-      date: "April 2023",
-      super98: "3.01",
-      special95: "2.90",
-      ePlus91: "2.82",
-      diesel: "3.03",
-    },
-    {
-      date: "May 2023",
-      super98: "3.16",
-      special95: "3.05",
-      ePlus91: "2.97",
-      diesel: "2.91",
-    },
-    {
-      date: "June 2023",
-      super98: "2.95",
-      special95: "2.84",
-      ePlus91: "2.76",
-      diesel: "2.68",
-    },
-    {
-      date: "July 2023",
-      super98: "3.00",
-      special95: "2.89",
-      ePlus91: "2.81",
-      diesel: "2.76",
-    },
-    {
-      date: "August 2023",
-      super98: "3.14",
-      special95: "3.02",
-      ePlus91: "2.95",
-      diesel: "2.95",
-    },
-  ];
-
   return (
     <Container fluid className="table-container">
       <Row>
