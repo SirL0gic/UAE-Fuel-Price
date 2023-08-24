39ed6c9366cbbccd2e5aa3373292c01a09226d6b
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-08-24 10:47:57 +0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     try {
       var dev = "http://localhost:4000";
       var production = "https://api.fuelwatch.xyz"
-      axios.defaults.baseURL = production;
+      axios.defaults.baseURL = dev;
 
       const current_response = await axios.get("/api/all-fuel-data");
 
